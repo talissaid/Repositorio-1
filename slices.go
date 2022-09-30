4eef9c2cedab1f76b89b1dfd199e4626b8052fb4
Gabri-Ellie <gabrielli.vicente@madeiramadeira.com.br>
2022-09-30 14:56:53 -0300

    teste de slices do go

---
+++ slices.go
@@ -0,0 +1,20 @@
+package main
+
+import "log"
+
+func main() {
+	slice := make([]int, 5, 12 )
+
+	slice[0],slice[1],slice[2],slice[3],slice[4] = 1,2,3,4,5
+
+	slice = append(slice, 6)
+	slice = append(slice, 7)
+	slice = append(slice, 8)
+	slice = append(slice, 9)
+	slice = append(slice, 10)
+	slice = append(slice, 11)
+
+	log.Println(slice, len(slice), cap(slice))
+
+
+}
